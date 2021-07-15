a6b412d7e7426b7944f98fac4ffa36846ea0b6e0
amartya <amartyasinha918@gmail.com>
2021-07-15 11:39:33 +0530

    Swing

---
+++ src/ButtonColorChangeSwing.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ButtonColorChangeSwing implements ActionListener {
+    JFrame frame;
+    Button redButton, blueButton;
+
+    ButtonColorChangeSwing() {
+        frame = new JFrame("Button Color Change");
+        redButton = new Button("RED");
+        redButton.setBounds(120, 90, 150, 20);
+        redButton.addActionListener(this);
+        frame.add(redButton);
+
+        blueButton = new Button("BLUE");
+        blueButton.setBounds(120, 150, 150, 20);
+        blueButton.addActionListener(this);
+        frame.add(blueButton);
+
+        frame.setSize(400, 300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == redButton)
+            frame.getContentPane().setBackground(Color.RED);
+        if (ae.getSource() == blueButton)
+            frame.getContentPane().setBackground(Color.BLUE);
+    }
+
+    public static void main(String[] args) {
+        new ButtonColorChangeSwing();
+    }
+}
